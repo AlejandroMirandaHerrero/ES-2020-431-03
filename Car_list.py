95400bb89b5b71289b99def393dd37e9d0df1d89
victorfr16 <65301808+victorfr16@users.noreply.github.com>
2020-05-22 09:42:20 +0200

    Update Car_list.py

--- Car_list.py
+++ Car_list.py
@@ -1,2 +1,24 @@
 # -*- coding: utf-8 -*-
 
+  
+from Cars import Cars 
+
+class Car_list:
+
+    def __init__(self):
+        self.coches=[]
+        self.preutotal=0
+        self.numcoches=0
+        
+    def agregarcoche(self,c: Cars):
+        self.coches.append(c)
+        self.numcoches+=1
+        self.preutotal+=c.preu_persona
+        
+    def elimnarcoche(self,coche):
+        for i in self.coches:
+            if i.codi == coche:
+                self.numcoches -= 1
+                self.preutotal -= i.preu
+                self.coches.remove(i)
+
