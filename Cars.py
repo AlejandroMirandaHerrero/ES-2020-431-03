9549a455792dc30ac21535bdf02f321dd084bfaa
victorfr16 <65301808+victorfr16@users.noreply.github.com>
2020-05-22 09:41:00 +0200

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -1,7 +1,8 @@
 class Cars:
 
-    def __init__(self, codi_cotxe, marcac, recollida, dies):
+    def __init__(self, codi_cotxe, marcac, recollida, dies, preu):
         self.codi = codi_cotxe
         self.marca = marcac
         self.lloc = recollida
         self.durada= dies
+        self.preu = preu
