417394598864a7e2f2145b26b9708654bf9aec96
victorfr16 <65301808+victorfr16@users.noreply.github.com>
2020-05-22 09:23:34 +0200

    Add files via upload

---
+++ Alojamiento.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from User import User
+from Hotel_list import Hotel_list
+
+
+class Alojamiento:
+
+    def __init__(self):
+        pass
+
+    def confirm_reserve_vuelo(self, user: User, hotel: Hotel_list) -> bool:
+        return True
+    
+    
+    def getlistvuelo(self,alojamiento):
+        return []
+    
+   
+    
