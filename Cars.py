90f7f57d66a4524ab2ba6b05af34f040ac8f1a75
JonathanMolinaM <64977631+JonathanMolinaM@users.noreply.github.com>
2020-05-22 11:05:16 +0200

    Update Cars.py

--- Cars.py
+++ Cars.py
@@ -1,8 +1,9 @@
 class Cars:
 
-    def __init__(self, codi_cotxe, marcac, recollida, dies, preu):
+    def __init__(self, codi_cotxe, marcac,preu_dia, recollida, dies):
         self.codi = codi_cotxe
         self.marca = marcac
         self.lloc = recollida
         self.durada= dies
-        self.preu = preu
+        self.preu_dia=preu_dia
+    
