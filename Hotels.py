c6eb054883d5b34e1673fa7c621e0a362f2359c3
victorfr16 <65301808+victorfr16@users.noreply.github.com>
2020-05-23 10:44:48 +0200

    Update Hotels.py

--- Hotels.py
+++ Hotels.py
@@ -1,8 +1,9 @@
 class Hotels:
 
-    def __init__(self, codi_hotel, nomh, nh, numeroha, dies):
+    def __init__(self, codi_hotel, nomh, nh, numeroha, dies, preu_persona):
         self.codi = codi_hotel
         self.nom = nomh
         self.numerohostes = nh
         self.numerohabitacions= numeroha
         self.durada= dies
+        self.preu_persona = preu_persona
