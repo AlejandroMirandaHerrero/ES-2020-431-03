e832a3ecde2d8f5ceb5d2114aa0fb3cae7ea4296
JonathanMolinaM <64977631+JonathanMolinaM@users.noreply.github.com>
2020-05-23 15:33:51 +0200

    Update PaymentData.py

--- PaymentData.py
+++ PaymentData.py
@@ -5,8 +5,24 @@ class PaymentData:
         self.titular=titular
         self.numero_t=numero_t
         self.codigo_t=codigo_t
+        self.correcto=True
         
+        if type(self.tipo)!=str or type(self.titular)!=str or type(self.numero_t)!=int or type(self.codigo_t)!=int:
+            print("Error de format.")
+            self.correcto=False
+            
+        if self.tipo=="" or self.titular=="" or self.numero_t== None or self.codigo_t==None:
+            print("Falten dades.")
+            self.correcto=False
+        
+        if len(str(numero_t))!=10 or len(str(codigo_t))!=4:
+            print("Error de format al numero de tarjeta o codi de seguretat.")
+            self.correcto= False
+            
     def get_datapayment(self):
-        return self.tipo, self.titular, self.numero_t, self.codigo_t
+        if self.correcto==True:
+            return self.tipo, self.titular, self.numero_t, self.codigo_t
+        else:
+            return False
 
 
