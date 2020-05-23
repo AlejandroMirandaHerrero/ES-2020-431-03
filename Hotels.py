c188eb5a1754139eba4b967ad23022abca8c419d
JonathanMolinaM <64977631+JonathanMolinaM@users.noreply.github.com>
2020-05-23 11:50:12 +0200

    Update Hotels.py

--- Hotels.py
+++ Hotels.py
@@ -1,8 +1,9 @@
 class Hotels:
 
-    def __init__(self, codi_hotel, nomh, nh, numeroha, dies):
+    def __init__(self, codi_hotel, nomh, nh, numeroha, dies,preupersona):
         self.codi = codi_hotel
         self.nom = nomh
         self.numerohostes = nh
         self.numerohabitacions= numeroha
         self.durada= dies
+        self.preu_persona=preupersona
